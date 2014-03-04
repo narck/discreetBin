35a0c4f3cdfc00f4b248ef1c7d97636cace1597d
Nicolas Arkkila <narck@narchiembp.local>
2014-03-04 14:40:34 +0200

    views, basic functions working

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var crypto = require('crypto');
 var app = express();
 
+var redis = require("redis"),
+        client = redis.createClient();
 
 /* CONFIGURATION */
 app.use(express.urlencoded());
@@ -15,6 +18,11 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
+var redisErrorHandle = function() {
+	client.on("error", function (err) {
+		res.send("Redis error"); 
+	});
+};
 
 /* ROUTES */
 
@@ -22,9 +30,32 @@ app.get('/', function(req, res) {
 	res.sendfile('./views/index.html');
 });
 
-app.post('/create', function(req, res){
-	var data = req.body.kek;
-    console.log(data);
-    res.redirect('/');
+app.get('/create', function(req, res) {
+	res.sendfile('./views/create.html');
 });
 
+app.post('/create', function(req, res){
+	crypto.randomBytes(48, function(err, buffer) {
+		var token = buffer.toString('base64');
+		
+		var hash = crypto.createHmac('sha1', token).update(req.body.paste).digest('hex');
+
+		client.set(hash, req.body.paste, redis.print);
+		client.get(hash, function (err, reply) {
+			console.log(reply.toString());
+			res.end('Your hash is ' + hash);
+		});
+	});
+});
+
+app.get("/show/:id?", function (req, res) {
+	client.get(req.params.id, function(err, reply) {
+
+		console.log(err);		
+		if (reply===null) {
+			res.end("Please supply a valid id!");
+		} else {
+			res.send("The key value = "+reply.toString());	
+		};
+	});
+});
